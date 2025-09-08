05f7567deef0645a032bed481a905f4fe542fb9d
fujitani sora <fujitanisora0414@gmail.com>
2025-09-08 17:51:38 +0900

    internal

---
+++ lib/r_format.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "r_format/version"
+require_relative "r_format/r_format"
+
+module RFormat
+  class Error < StandardError; end
+  # Your code goes here...
+end
